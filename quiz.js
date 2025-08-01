8f704be0ba5465957a53761fbc041360f63e88bc
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-01 12:33:11 +0300

    added an eventlistener to the submit button

--- quiz.js
+++ quiz.js
@@ -1,11 +1,14 @@
 function checkAnswer(){
     const correctAnswer = "4";
-    const userAnswer = document.querySelector('input[name="quiz"]:checked').value
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    const feedback = document.getElementById("feedback");
     if (userAnswer === correctAnswer){
-        const feedback = document.getElementById("feedback");
         feedback.textContent = "Correct! Well done.";
     }
-    else(userAnswer !== correctAnswer){
+    else{
         feedback.textContent = "That's incorrect. Try again!";
     };
 };
+const button = document.getElementById("submit-answer");
+button.addEventListener("click", checkAnswer);
+
