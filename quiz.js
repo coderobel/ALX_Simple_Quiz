ed3027808c181c56c2589a3cb6594559a11e214f
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-01 12:24:05 +0300

    updated quiz.js to retrieve and compare Users answer with correct answer

--- quiz.js
+++ quiz.js
@@ -1,11 +1,11 @@
 function checkAnswer(){
     const correctAnswer = "4";
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value
-    if(userAnswer === correctAnswer){
+    if (userAnswer === correctAnswer){
         const feedback = document.getElementById("feedback");
         feedback.textContent = "Correct! Well done.";
     }
-    elif(userAnswer !== correctAnswer){
+    else(userAnswer !== correctAnswer){
         feedback.textContent = "That's incorrect. Try again!";
     };
 };
