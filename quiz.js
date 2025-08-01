952530d8f1c4238f563cebef0e4e0e94a28a4c26
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-01 12:22:19 +0300

    updated quiz.js to retrieve and compare Users answer with correct answer

--- quiz.js
+++ quiz.js
@@ -1,11 +1,11 @@
 function checkAnswer(){
     const correctAnswer = "4";
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value
-    if(userAnswer == correctAnswer){
+    if(userAnswer === correctAnswer){
         const feedback = document.getElementById("feedback");
         feedback.textContent = "Correct! Well done.";
     }
-    elif(userAnswer != correctAnswer){
+    elif(userAnswer !== correctAnswer){
         feeedback.textContent = "That's incorrect. Try again!";
     }
 };
